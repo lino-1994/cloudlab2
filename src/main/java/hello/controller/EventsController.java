d0e1e514644636eb4c6d93fa26a878e05a80ca4d
Pasquale_Palmieri <peppoprotoco@hotmail.com>
2019-07-20 11:16:39 +0200

    primi servizi rest

--- src/main/java/hello/controller/EventsController.java
+++ src/main/java/hello/controller/EventsController.java
@@ -22,6 +22,34 @@ public class EventsController {
     public List<Event> fetchEvents() {
         return eventsRepository.findAll();
     }
+    
 
+    @PostMapping("/events")
+    public Event postEvents(@RequestBody Event event){
+    	eventsRepository.save(event);
+    	return event;
+    }
+    
+    @GetMapping("/events/search")
+    public Event searchEvent(@RequestParam(defaultValue = "1") long id) {
+        return eventsRepository.findById(id).get();
+    }
+    
+    @PutMapping ("/events/update/{id}")
+    public ResponseEntity<Object> updateEvent(@PathVariable long id, @RequestBody Event newEvent) {
+    	Optional<Event> optEvent= eventsRepository.findById(id);
+    	if (optEvent.isPresent()) {
+    		newEvent.setId(id);
+    		eventsRepository.save(newEvent);
+    		return ResponseEntity.noContent().build();
+    	}
+    	return ResponseEntity.notFound().build();
+    	
+    }
+    
+    @DeleteMapping ("/events/delete")
+    public void deleteEvents(@RequestParam long id){
+    	eventsRepository.deleteById(id);
+    }
 }
 
